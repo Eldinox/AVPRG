857d3127dfc32265a49d87437146825391e6365d
Eldinox <waldemar.g94@web.de>
2018-11-24 19:12:18 +0100

    read level data from file

---
+++ wewillrockyou.js
@@ -0,0 +1,6 @@
+data = {"gaps":["blue-0", "blue-114", "red-228",
+"blue-456", "blue-570", "red-684",
+"blue-912", "blue-1026", "red-1140",
+"blue-1368", "blue-1482", "red-1596",
+"blue-1824", "blue-1938", "red-2052",
+"blue-2280", "blue-2394", "red-2508"]};
